cf293ff1fdc4562ccc57e21fc4cbae43846be049
Rawan <rewanmohamed869@gmail.com>
2024-01-01 16:19:44 +0200

    Adding checkIfExist and updateStatus DB functions

--- src/Database/DataAccessLayer.java
+++ src/Database/DataAccessLayer.java
@@ -5,10 +5,41 @@
  */
 package Database;
 
+import dto.PlayerDTO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.ClientDriver;
+
 /**
  *
  * @author HP
  */
 public class DataAccessLayer {
+    public static boolean checkIfPlayerExist(PlayerDTO player) throws SQLException{
+        boolean exist;
+        DriverManager.registerDriver(new ClientDriver());
+        Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToe","root","root");
+        PreparedStatement prepareStatement = connection.prepareStatement("SELECT * FROM Player WHERE USERNAME=? " , ResultSet.TYPE_SCROLL_SENSITIVE , ResultSet.CONCUR_READ_ONLY);
+        prepareStatement.setString(1, player.getUsername());
+        ResultSet result = prepareStatement.executeQuery();
+        if(result != null){
+            exist = true;
+        }else{
+            exist=false;
+        }
+        return exist;
+    }
     
+    public static int updateStatus(PlayerDTO player) throws SQLException{
+        int result;
+        DriverManager.registerDriver(new ClientDriver());
+        Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToe","root","root");
+        PreparedStatement prepareStatement = connection.prepareStatement("UPDATE Player SET STATUS=online WHERE USERNAME=?");
+        prepareStatement.setString(1, player.getUsername());
+        result = prepareStatement.executeUpdate();
+        return result;
+    }
 }
